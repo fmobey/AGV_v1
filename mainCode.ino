41abed26c4b33dc613afa3609b1489562be26f12
Furkan metin OĞUZ <dumstp1@gmail.com>
2022-07-06 15:59:09 +0300

    0606

--- mainCode.ino
+++ mainCode.ino
@@ -3,21 +3,29 @@
 
 #define BATTERY_AVARAGE_READ_COUNT 10
 
-#define A_Palse 8
-#define A_Dir 10
-#define B_Palse 44
-#define B_Dir 46
-#define C_Palse 13
-#define C_Dir 27
-#define D_Palse 9
-#define D_Dir 11
+#define APWM 12
+#define ADIR 10
+#define ADIRFW 8
+
+#define BPWM 46
+#define BDIR 48
+#define BDIRFW 30
+
+#define CPWM 45
+#define CDIR 25
+#define CDIRFW 23
+
+#define DPWM 13
+#define DDIR 11
+#define DDIRFW 9
+
 
 //rgb pin
-#define RED_PIN 5
-#define GREEN_PIN 6
-#define BLUE_PIN 7
+#define RED_PIN 2
+#define GREEN_PIN 3
+#define BLUE_PIN 4
 //buzzer pin
-#define BUZZER_PIN 8
+#define BUZZER_PIN 5
 //millis function
 unsigned long oldTime=0;
 unsigned long newTime;
@@ -29,15 +37,17 @@ unsigned long adc_millis = 0;
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-#define ILERI_PIN A6
-#define GERI_PIN A4
-#define SAGA_PIN A0
-#define SOLA_PIN A2
-#define SOLA_DON_PIN A1
-#define SAGA_DON_PIN A3
+#define ILERI_PIN 43
+#define GERI_PIN 41
+#define SAGA_PIN 7
+#define SOLA_PIN 44
+#define SOLA_DON_PIN 29
+#define SAGA_DON_PIN 31
 
-#define ILERI_GIT A6
-#define GERI_GIT A4
+#define ANALOG_PIN_BUTTON A0
+
+#define VBATTADC 14
+
 int ileriAnalogValue = 256;
 int geriAnalogValue = 512;
 
@@ -61,10 +71,10 @@ void setup()
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
 
-  pinMode(A_Palse, OUTPUT);
-  pinMode(B_Palse, OUTPUT);
-  pinMode(C_Palse, OUTPUT);
-  pinMode(D_Palse, OUTPUT);
+  pinMode(APWM, OUTPUT);
+  pinMode(BPWM, OUTPUT);
+  pinMode(CPWM, OUTPUT);
+  pinMode(DPWM, OUTPUT);
 
   pinMode(ILERI_PIN, INPUT);
   pinMode(GERI_PIN, INPUT);
@@ -76,15 +86,15 @@ void setup()
   pinMode(ILERI_GIT, INPUT);
   pinMode(GERI_GIT, INPUT);
 
-  pinMode(A_Dir, OUTPUT);
-  pinMode(B_Dir, OUTPUT);
-  pinMode(C_Dir, OUTPUT);
-  pinMode(D_Dir, OUTPUT);
+  pinMode(ADIR, OUTPUT);
+  pinMode(BDIR, OUTPUT);
+  pinMode(CDIR, OUTPUT);
+  pinMode(DDIR, OUTPUT);
 
-  digitalWrite(A_Dir, HIGH);
-  digitalWrite(B_Dir, HIGH);
-  digitalWrite(C_Dir, HIGH);
-  digitalWrite(D_Dir, HIGH);
+  digitalWrite(ADIR, HIGH);
+  digitalWrite(BDIR, HIGH);
+  digitalWrite(CDIR, HIGH);
+  digitalWrite(DDIR, HIGH);
 
   lcd.clear();
 
@@ -100,8 +110,8 @@ void loop()
   int SAGA_DON = digitalRead(SAGA_DON_PIN);
   int SOLA_DON = digitalRead(SOLA_DON_PIN);
   //istasyon butonlarının durumlarını okuyup gösteriyoruz
-  int ILERI_GIT_BUTTON = analogRead(ILERI_GIT);
-  int GERI_GIT_BUTTON = analogRead(GERI_GIT);
+  int ANALOG_GIT_BUTTON = analogRead(ANALOG_PIN_BUTTON);
+
 
   TIME_NOW = millis();
   if (TIME_NOW - adc_millis > 1)
@@ -117,45 +127,45 @@ void loop()
   {
 
     RotateWheels(false, false, false, true, false, false);
-    rgbController(true, false, false);
+    rgbController(true, false, false, false);
     //Serial.println("SAGA");
   }
   else if (GERI_TRY == LOW || ILERI_GIT_BUTTON == ileriAnalogValue)
   {
     RotateWheels(true, false, false, false, false, false);
-    rgbController(true, false, false);
+    rgbController(true, false, false, false);
    // Serial.println("GERI");
   }
   else if (ILERI_TRY == LOW )
   {
 
     RotateWheels(false, true, false, false, false, false);
-    rgbController(true, false, false);
+    rgbController(true, false, false, false);
    //Serial.println("ILERI");
   }
   else if (SOLA_TRY == LOW)
   {
 
     RotateWheels(false, false, true, false, false, false);
-    rgbController(false, true, false);
+    rgbController(false, true, false, false);
    // Serial.println("SOLA");
   }
    else if (SOLA_DON == LOW)
     {
     
     RotateWheels(false, false, false, false, true, false);
-    rgbController(false, true, false);
+    rgbController(false, true, false, false);
      // Serial.println("SOLA_DON");
     }
     else if (SAGA_DON == LOW)
     {
-    rgbController(false, true, false);
+    rgbController(false, true, false, false);
     RotateWheels(false, false, false, false, false, true);
      // Serial.println("SAGA_DON");
-    }else if(GERI_GIT_BUTTON == geriAnalogValue){
+    }else if(ANALOG_GIT_BUTTON == geriAnalogValue){
 
 
-    }else if(ILERI_GIT_BUTTON == ileriAnalogValue){
+    }else if(ANALOG_GIT_BUTTON == ileriAnalogValue){
 
 
     }
@@ -165,6 +175,16 @@ void loop()
     }
 
 }
+//10 munite timer stanby mode 
+void timer_standby()
+{
+  if (TIME_NOW - oldTime > 10000)
+  {
+
+  }
+}
+
+
 
 
 void RotateWheels(bool ileri, bool geri, bool sag, bool sol, bool soldon, bool sagdon)
@@ -172,45 +192,69 @@ void RotateWheels(bool ileri, bool geri, bool sag, bool sol, bool soldon, bool s
   
   if (ileri == true)
   {
-    digitalWrite(A_Dir, HIGH);
-    digitalWrite(B_Dir, LOW);
-    digitalWrite(C_Dir, LOW);
-    digitalWrite(D_Dir, HIGH);
+    digitalWrite(ADIR, HIGH);
+    digitalWrite(ADIRFW, LOW);
+    digitalWrite(BDIR, LOW);
+    digitalWrite(BDIRFW, HIGH);
+    digitalWrite(CDIR, LOW);
+    digitalWrite(CDIRFW, HIGH);
+    digitalWrite(DDIR, HIGH);
+    digitalWrite(DDIRFW, LOW);
   }
   else if (geri == true)
   {
-    digitalWrite(A_Dir, LOW);
-    digitalWrite(B_Dir, HIGH);
-    digitalWrite(C_Dir, HIGH);
-    digitalWrite(D_Dir, LOW);
+    digitalWrite(ADIR, LOW);
+    digitalWrite(ADIRFW, HIGH);
+    digitalWrite(BDIR, HIGH);
+    digitalWrite(BDIRFW, LOW);
+    digitalWrite(CDIR, HIGH);
+    digitalWrite(CDIRFW, LOW);
+    digitalWrite(DDIR, LOW);
+    digitalWrite(DDIRFW, HIGH);
   }
   else if (sag == true)
   {
-    digitalWrite(A_Dir, LOW);
-    digitalWrite(B_Dir, LOW);
-    digitalWrite(C_Dir, HIGH);
-    digitalWrite(D_Dir, HIGH);
+    digitalWrite(ADIR, LOW);
+    digitalWrite(ADIRFW, HIGH);
+    digitalWrite(BDIR, LOW);
+    digitalWrite(BDIRFW, HIGH);
+    digitalWrite(CDIR, HIGH);
+    digitalWrite(CDIRFW, LOW);
+    digitalWrite(DDIR, HIGH);
+    digitalWrite(DDIRFW, LOW);
   }
   else if (sol == true)
   {
-    digitalWrite(A_Dir, HIGH);
-    digitalWrite(B_Dir, HIGH);
-    digitalWrite(C_Dir, LOW);
-    digitalWrite(D_Dir, LOW);
+    digitalWrite(ADIR, HIGH);
+    digitalWrite(ADIRFW, LOW);
+    digitalWrite(BDIR, HIGH);
+    digitalWrite(BDIRFW, LOW);
+    digitalWrite(CDIR, LOW);
+    digitalWrite(CDIRFW, HIGH);
+    digitalWrite(DDIR, LOW);
+    digitalWrite(DDIRFW, HIGH);
   }
   else if (soldon == true)
   {
-    digitalWrite(A_Dir, HIGH);
-    digitalWrite(B_Dir, LOW);
-    digitalWrite(C_Dir, HIGH);
-    digitalWrite(D_Dir, LOW);
+    digitalWrite(ADIR, HIGH);
+    digitalWrite(ADIRFW, LOW);
+    digitalWrite(BDIR, LOW);
+    digitalWrite(BDIRFW, HIGH);
+    digitalWrite(CDIR, HIGH);
+    digitalWrite(CDIRFW, LOW);
+    digitalWrite(DDIR, LOW);
+    digitalWrite(DDIRFW, HIGH);
   }
   else if (sagdon == true)
   {
-    digitalWrite(A_Dir, LOW);
-    digitalWrite(B_Dir, HIGH);
-    digitalWrite(C_Dir, LOW);
-    digitalWrite(D_Dir, HIGH);
+    digitalWrite(ADIR, LOW);
+    digitalWrite(ADIRFW, HIGH);
+    digitalWrite(BDIR, HIGH);
+    digitalWrite(BDIRFW, LOW);
+    digitalWrite(CDIR, LOW);
+    digitalWrite(CDIRFW, HIGH);
+    digitalWrite(DDIR, HIGH);
+    digitalWrite(DDIRFW, LOW);
   }
   else
   {
@@ -219,6 +263,7 @@ void RotateWheels(bool ileri, bool geri, bool sag, bool sol, bool soldon, bool s
 
 void LCD_MESSAGE(int hiz)
 {
+  //en son yapılacak 
 lcd.setCursor(0, 0);
 lcd.print("      OMNIMAN       ");
 lcd.setCursor(0, 1);
@@ -245,14 +290,14 @@ float vBattRead()
     int adc_value = 0;
     for (int i = 0; i < BATTERY_AVARAGE_READ_COUNT; i++)
     {
-        adc_value += analogRead(Vbatt_ADC);
+        adc_value += analogRead(VBATTADC);
     }
     adc_value = adc_value / BATTERY_AVARAGE_READ_COUNT;
     float Vbatt = (adc_value * 5) / 1024;
     return Vbatt;
 }
 
-void rgbController(bool WakeUpLight, bool straightLight, bool lateralLight)
+void rgbController(bool WakeUpLight, bool straightLight, bool lateralLight,bool lidarLight)
 {
 if(WakeUpLight == true)
 {
@@ -265,6 +310,10 @@ straightLightFade();
 else if(lateralLight == true)
 {
 lateralLightFade();
+}
+else if(lidarLight == true)
+{
+lidarLightFade();
 }
 else
 {
@@ -330,6 +379,21 @@ greenCount--;
     }
     oldTime = newTime;
 }}
+
+void lidarLightFade(){
+    newTime = millis(); 
+if(newTime-oldTime > 1000) {
+for(int i = 0; i < 127; i++){
+analogWrite(RED_PIN, i);
+}
+    }
+for(int i = 127; i < 0; i--){
+analogWrite(RED_PIN, i);
+
+    }
+    oldTime = newTime;
+}
+
 void buzzerFlipFlop(){
     newTime = millis();
 if(newTime-oldTime > 1000) {
